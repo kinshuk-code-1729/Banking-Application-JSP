ae1ff3fddfae1d6299c17334f327fa3ba5e9c66b
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2023-08-09 22:48:08 +0530

    Add files via upload

---
+++ src/java/TestRandom.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public class TestRandom {
+    public static void main(String args[]){
+        Random r1 = new Random();
+        System.out.println(r1.nextInt());
+    }
+}
